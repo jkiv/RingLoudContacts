2dd7c389aef766373892ade383041605d885e905
Jon Kivinen <jlkivine@lakeheadu.ca>
2010-11-07 17:42:07 -0500

    Fixed phone volume resetting without a phone call.

--- src/ca/jkiv/RingLoudContacts/VolumeControl.java
+++ src/ca/jkiv/RingLoudContacts/VolumeControl.java
@@ -15,6 +15,7 @@ public class VolumeControl
 	private static final String VOLUME_FILE = "RingLoudContacts_Volume";
 	private static final String VOLUME_MODE_PREF = "VolumeMode";
 	private static final String VOLUME_LEVEL_PREF = "VolumeLevel";
+	private static final String VOLUME_HAS_MAXED = "VolumeHasMaxed";
 	
 	// Default remembered state is silent (this is the assumed operation)
 	private static final int VOLUME_MODE_PREF_DEF = AudioManager.RINGER_MODE_SILENT;
@@ -30,8 +31,12 @@ public class VolumeControl
 	 */
 	public static void resumeVolume(Context context)
 	{
-		AudioManager audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
 		SharedPreferences sharedPreferences = context.getSharedPreferences(VOLUME_FILE, 0);
+		
+		// Only resume volume if VOLUME_HAS_MAXED is true
+		if (!sharedPreferences.getBoolean(VOLUME_HAS_MAXED, false)) return;
+		
+		AudioManager audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
 		
 		int ringerMode = sharedPreferences.getInt(VOLUME_MODE_PREF, VOLUME_MODE_PREF_DEF);
 		int volumeLevel = sharedPreferences.getInt(VOLUME_LEVEL_PREF, VOLUME_LEVEL_PREF_DEF);
@@ -39,6 +44,11 @@ public class VolumeControl
 		
 		audioManager.setRingerMode(ringerMode);
 		audioManager.setStreamVolume(stream, volumeLevel, flags);
+		
+		// Don't allow resumeVolume() again until a call to maxVolume()
+		Editor editor = sharedPreferences.edit();
+		editor.putBoolean(VOLUME_HAS_MAXED, false);
+		editor.commit();
 	}
 
 	/**
@@ -64,7 +74,7 @@ public class VolumeControl
 	 * @param context
 	 */
 	public static void maxVolume(Context context)
-	{
+	{	
 		AudioManager audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
 		
 		int stream = AudioManager.STREAM_RING;
@@ -72,6 +82,12 @@ public class VolumeControl
 		int flags = 0;
 
 		audioManager.setStreamVolume(stream, maxVolume, flags);
-		audioManager.setRingerMode(AudioManager.RINGER_MODE_NORMAL);		
+		audioManager.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
+		
+		// Allow resumeVolume()
+		SharedPreferences sharedPreferences = context.getSharedPreferences(VOLUME_FILE, 0);
+		Editor editor = sharedPreferences.edit();
+		editor.putBoolean(VOLUME_HAS_MAXED, true);
+		editor.commit();
 	}
 }
