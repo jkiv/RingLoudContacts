ae4f979ee031fa507561a6a0990f56e8afa0d7e9
Jon Kivinen <jlkivine@lakeheadu.ca>
2010-12-12 00:10:46 -0500

    Linkified about dialog.

--- src/ca/jkiv/RingLoudContacts/SettingsActivity.java
+++ src/ca/jkiv/RingLoudContacts/SettingsActivity.java
@@ -1,11 +1,20 @@
 package ca.jkiv.RingLoudContacts;
 
 import android.app.AlertDialog;
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
+import android.text.SpannableString;
+import android.text.method.LinkMovementMethod;
+import android.text.util.Linkify;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
 
 /**
  * The activity for changing the settings.
@@ -29,23 +38,45 @@ public class SettingsActivity extends PreferenceActivity
         
         // Use preferences.xml to define preference schema
         addPreferencesFromResource(R.xml.preferences);
+
+        // Show version number
+        Preference aboutItem = findPreference("_AboutLink");
+        
+        try
+        {
+     	   PackageInfo packageInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+     	   aboutItem.setSummary("Version " + packageInfo.versionName);
+        } 
+        catch (NameNotFoundException e)
+        {
+     	   aboutItem.setSummary("Version ???");
+        }
         
         // When we click the "About" item, we want to display an about dialog.
-        Preference editWhitelistLink = (Preference) findPreference("_AboutLink");
-        editWhitelistLink.setOnPreferenceClickListener(new OnPreferenceClickListener()
+        aboutItem.setOnPreferenceClickListener(new OnPreferenceClickListener()
         {
             public boolean onPreferenceClick(Preference preference)
             {
             	// Create about dialog
+            	LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        		View layout = inflater.inflate(R.layout.aboutdialog, null);
+        		TextView aboutTextView = (TextView) layout.findViewById(R.id.AboutDialog_TextView);
+        		
+            	final SpannableString s = new SpannableString(getText(R.string.AboutDialog_MessageText));
+        		Linkify.addLinks(s, Linkify.WEB_URLS);
+        		aboutTextView.setText(s);
+        		aboutTextView.setMovementMethod(LinkMovementMethod.getInstance());
 
-            	new AlertDialog.Builder(SettingsActivity.this)
-                .setTitle(R.string.About_Title)
-                .setMessage(R.string.About_Message)
-                .setIcon(R.drawable.ic_launcher_ring_loud_contacts)
-                .show();
-                
+        		new AlertDialog.Builder(SettingsActivity.this)
+        		    .setTitle(R.string.AboutDialog_TitleText)
+        		    .setIcon(R.drawable.ic_launcher_ring_loud_contacts)
+        		    .setPositiveButton(R.string.Dialog_OKText, null)
+        		    .setView(layout)
+        		    .show();
+            	
                 return true;
             }
         });
+
     }
 }
